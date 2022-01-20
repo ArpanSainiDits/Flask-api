ddbe69d5a707fb3d0cd7979e9391c45c949c05df
arpan saini <arpansainiwins@gmail.com>
2022-01-20 17:34:33 +0530

    flask api

--- routes.py
+++ routes.py
@@ -1,14 +1,22 @@
 from flask import Flask
-from flask_restplus import Api
+from flask_restful import Api, Resource
+from db import app
+
+from app import delEmp, getEmp, getEmpId, addEmp, updateEmp, delEmp, addUser
 
 
-from .app import add_employee
 
 
 
-app = Flask(__name__)
-def generate_route(app):
-    
-    api = Api(app)
-    
-    api.add_resource(add_employee, '/api/employee')
+api = Api(app)
+
+api.add_resource(getEmp, '/api/getemp')
+api.add_resource(getEmpId, '/api/getemp/<id>')
+api.add_resource(addEmp, '/api/addemp')
+api.add_resource(updateEmp, '/api/updateemp')
+api.add_resource(delEmp, '/api/delemp')
+api.add_resource(addUser, '/api/adduser')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
