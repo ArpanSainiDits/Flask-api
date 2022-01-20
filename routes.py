a48830796ce38677f81621f158198cacb6dbeac6
arpan saini <arpansainiwins@gmail.com>
2022-01-20 19:20:20 +0530

    flask api

--- routes.py
+++ routes.py
@@ -1,22 +1,17 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
 from db import app
 
-from app import delEmp, getEmp, getEmpId, addEmp, updateEmp, delEmp, addUser
+from services.views import delEmp, getEmp, getEmpId, addEmp, updateEmp, delEmp, addUser
 
 
+def view_routes(app):
+    api = Api(app)
 
+    api.add_resource(getEmp, '/api/getemp')
+    api.add_resource(getEmpId, '/api/getemp/<id>')
+    api.add_resource(addEmp, '/api/addemp')
+    api.add_resource(updateEmp, '/api/updateemp')
+    api.add_resource(delEmp, '/api/delemp')
+    api.add_resource(addUser, '/api/adduser')
 
-
-api = Api(app)
-
-api.add_resource(getEmp, '/api/getemp')
-api.add_resource(getEmpId, '/api/getemp/<id>')
-api.add_resource(addEmp, '/api/addemp')
-api.add_resource(updateEmp, '/api/updateemp')
-api.add_resource(delEmp, '/api/delemp')
-api.add_resource(addUser, '/api/adduser')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
